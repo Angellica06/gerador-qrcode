b8dddd703e8863a0c01b93bd34ab88b747b31f3e
Angélica Anjos <angellica.anjos@hotmail.com>
2025-03-05 15:04:52 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import prompt from "prompt";
+import mainPrompt from "./prompts/prompt-main.js";
+import createQRCODE from "./services/qr-code/create.js";
+import cretePassword from "./services/password/create.js";
+
+async function main() {
+    prompt.get(mainPrompt, async (err, choose) => {
+        if (choose.select == 1) await createQRCODE();
+        if (choose.select == 2) await cretePassword();
+    });
+
+    prompt.start();    
+}
+
+main();
